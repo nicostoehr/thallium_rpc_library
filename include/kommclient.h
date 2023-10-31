39bd0467b97b29ed0b5f417eb6e9e38308ce0671
Nico Stöhr <58736331+nicostoehr@users.noreply.github.com>
2023-10-31 17:02:31 +0100

    Add files via upload

---
+++ include/kommclient.h
@@ -0,0 +1,36 @@
+#ifndef CUSTOM_CLIENT_HPP
+#define CUSTOM_CLIENT_HPP
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <thallium.hpp>
+#include <vector>
+#include <thallium/serialization/stl/string.hpp>
+#include <sys/stat.h>
+#include <map>
+#include "custom_struct.hpp" // Include your custom_struct.hpp here
+
+#define CONF_FILENAME "client.conf"
+
+namespace tl = thallium;
+
+extern std::vector<std::string> server_list;
+extern tl::engine myEngine;
+extern std::map<int, std::pair<char*, int>> ufd_dictionary;
+extern unsigned int server_number;
+
+std::vector<std::string> read_conf();
+std::string hash_server(const char* path);
+std::pair<char*, int> ufd_lookup(int ufd);
+int calculate_ufd(int fd, const char* path);
+int get_ufd(int fd, const char* path);
+int komm_open(const char* path);
+int komm_stat(const char* path, struct stat* statbuf);
+int komm_unlink(const char* path);
+int komm_close(int ufd);
+ssize_t komm_pwrite(int ufd, const void* buf, size_t count, off_t offset);
+ssize_t komm_pread(int ufd, void* buf, size_t count, off_t offset);
+
+#endif // CUSTOM_CLIENT_HPP
